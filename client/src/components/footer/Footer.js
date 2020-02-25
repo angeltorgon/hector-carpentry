5d570375af4e8a42d77c35f02609b3ec3852b2eb
angel-torres <alas4224@gmail.com>
2020-02-25 12:21:30 -0600

    centered social links

--- client/src/components/footer/Footer.js
+++ client/src/components/footer/Footer.js
@@ -7,20 +7,11 @@ export default function Footer() {
     return (
         <div className="footer-container">
             <div className="footer-links-container" >
-                {/* <div className="page-links-container" >
-                    <a className="link" href="#">Home</a>
-                    <a className="link" href="#">About</a>
-                    <a className="link" href="#">Kitchens</a>
-                    <a className="link" href="#">Stairs</a>
-                    <a className="link" href="#">Beams</a>
-                </div> */}
-                <div className="social-links-container" >
-                    <div className="social-link">
-                        <img src="https://seeklogo.com/images/I/instagram-logo-A807AD378B-seeklogo.com.png"/>
-                    </div>
-                    <div className="social-link">
-                        <img src="https://image.flaticon.com/icons/png/512/59/59439.png"/>
-                    </div>
+                <div className="social-link">
+                    <img src="https://seeklogo.com/images/I/instagram-logo-A807AD378B-seeklogo.com.png"/>
+                </div>
+                <div className="social-link">
+                    <img src="https://image.flaticon.com/icons/png/512/59/59439.png"/>
                 </div>
             </div>
             <p>HectorCarpentry.com © 2020 </p>
