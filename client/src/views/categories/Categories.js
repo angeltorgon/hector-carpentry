ded75aa657350c0de41c6fdbd842c925484b29a1
angel-torres <alas4224@gmail.com>
2020-02-25 14:22:10 -0600

    added kitchen image to Category component

--- client/src/views/categories/Categories.js
+++ client/src/views/categories/Categories.js
@@ -2,11 +2,12 @@ import React from 'react';
 
 // STYLES
 import './styles/categories.scss';
+import Category from './Category';
 
 export default function Categories() {
     return (
         <div className="categories-container">
-            
+            <Category />
         </div>
     )
 }
