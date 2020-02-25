4da88cd28df733fa3b09171b257b5c6e20ab22b4
angel-torres <alas4224@gmail.com>
2020-02-25 12:07:08 -0600

    added reset to css and fixed a few styles

--- client/src/components/footer/Footer.js
+++ client/src/components/footer/Footer.js
@@ -6,7 +6,24 @@ import './styles/footer.scss';
 export default function Footer() {
     return (
         <div className="footer-container">
-            FOOTER 
+            <div className="footer-links-container" >
+                {/* <div className="page-links-container" >
+                    <a className="link" href="#">Home</a>
+                    <a className="link" href="#">About</a>
+                    <a className="link" href="#">Kitchens</a>
+                    <a className="link" href="#">Stairs</a>
+                    <a className="link" href="#">Beams</a>
+                </div> */}
+                <div className="social-links-container" >
+                    <div className="social-link">
+                        <img src="https://seeklogo.com/images/I/instagram-logo-A807AD378B-seeklogo.com.png"/>
+                    </div>
+                    <div className="social-link">
+                        <img src="https://image.flaticon.com/icons/png/512/59/59439.png"/>
+                    </div>
+                </div>
+            </div>
+            <p>HectorCarpentry.com © 2020 </p>
         </div>
     )
 }
