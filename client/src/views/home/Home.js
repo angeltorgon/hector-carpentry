1c94a90c0e1ce92bb864cd0469c3bc667df5c812
angel-torres <alas4224@gmail.com>
2020-02-26 08:43:24 -0600

    updated styles for cta button and fixed a few aligning issues

--- client/src/views/home/Home.js
+++ client/src/views/home/Home.js
@@ -12,7 +12,7 @@ export default function Home() {
     return (
         <div className="home-container">
             <Header/>
-            <h1 >ABOUT</h1>
+            <h1 className="about-h1">ABOUT</h1>
             <p className="bio">Hello! My name is Hector. I am a carpenter from the Houston area with over 15 years of experience. I have gained a wide variety of skills throughout my career including building <strong>kitchen cabinets</strong>, <strong>stairs</strong>, <strong>trim</strong>, and <strong>doors</strong>.</p>
             <div className="line-break"></div>
             <Categories />
