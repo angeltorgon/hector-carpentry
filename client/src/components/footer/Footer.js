04a85be49d3f8aa2ffe18da220f1f8e93620edf4
angel-torres <alas4224@gmail.com>
2020-02-27 12:14:33 -0600

    updated footer spacing and removed social media links for now

--- client/src/components/footer/Footer.js
+++ client/src/components/footer/Footer.js
@@ -6,16 +6,16 @@ import './styles/footer.scss';
 export default function Footer() {
     return (
         <div className="footer-container">
-            <p className="contact-message">Email - Text - Call - DM</p>
+            <p className="contact-message">Email - Text - Call</p>
             <p className="contact-info">hectorcarpentryhq@gmail.com </p>
-            <p className="contact-info">832-407-4390</p>
+            <p className="contact-info">(832)-407-4390</p>
             <div className="footer-links-container" >
-                <div className="social-link">
+                {/* <div className="social-link">
                     <img src="https://seeklogo.com/images/I/instagram-logo-A807AD378B-seeklogo.com.png"/>
                 </div>
                 <div className="social-link">
                     <img src="https://image.flaticon.com/icons/png/512/59/59439.png"/>
-                </div>
+                </div> */}
             </div>
             <p>hectorcarpentry.com©2020 All rights reserved.</p>
         </div>
