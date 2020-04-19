5a2ed4ab47a740cb009f53f29aa4e4b05444a260
angel-torres <alas4224@gmail.com>
2020-04-18 21:01:08 -0500

    updated image links and also added a flex-wrap for categories

--- client/src/components/categories/Categories.js
+++ client/src/components/categories/Categories.js
@@ -9,25 +9,25 @@ const categories = [
         category: "cabinets",
         description: "Custom Cabinets - Cabinet Door Installation - Islands - Pantries - Closets",
         images: [
-            "https://lda.lowes.com/is/image/Lowes/DP18-142907_1-1_dt_KitchenCabinetry_2col_hero-Image?scl=1",
-            "https://images.unsplash.com/photo-1579725942955-4d8377f8c66a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=967&q=80",
-            "https://images.unsplash.com/photo-1516888693095-f0e05366ddc6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80"
+            "https://drive.google.com/uc?id=1RTgropK3v_fc0CYIamRh5GGiJUHC723s",
+            "https://drive.google.com/uc?id=1RC_9Vc1xa6QgRzX-HTVakRlCnmXO-SEN",
+            "https://drive.google.com/uc?id=1RrLC5Ov_NLuDNjE88SSPnvaCN1CRL1ua"
         ]
     },
     {
         category: "stairs",
         description: "Step Installation - Ballister Installation - Hand Rail Installation",
         images: [
-            "https://images.unsplash.com/photo-1560170433-bc0adc32bccd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
+            "https://drive.google.com/uc?id=1Qt8NwqPlH2fEO4qdNQl3xhBmFLz871Xn",
             "https://images.unsplash.com/photo-1505334086731-6579ef4cf449?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
-            "https://images.unsplash.com/photo-1562764661-469bc3f331d1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
+            "https://drive.google.com/uc?id=1QhTrg636lNArp3yVDCxNxnhiwUbNn_mX"
         ]
     },
     {
         category: "trim",
         description: "Base Boards - Window Sills - Trim - Crown Moulding - Wainscoting",
         images: [
-            "https://wisconsinbarnhouse.weebly.com/uploads/1/1/9/8/11981788/3887675_orig.jpg",
+            "https://drive.google.com/uc?id=1RCZpWfvb1VihU9d5qG_Wlg9v6xL3r_qy",
             "https://i.pinimg.com/originals/bc/90/82/bc9082949bb0e555f2582a290cb0ec71.jpg",
             "https://live.staticflickr.com/2335/2182808920_21e7c4ca50_b.jpg"
         ]
