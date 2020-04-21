c776a6761533f848c5942d79686ac5fb787269ea
angel-torres <alas4224@gmail.com>
2020-04-21 17:31:12 -0500

    added left and right buttons to category component

--- client/src/components/categories/Category.js
+++ client/src/components/categories/Category.js
@@ -1,23 +1,49 @@
 import React, { useState, useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowAltCircleLeft, faArrowCircleRight, faArrowCircleLeft } from '@fortawesome/free-solid-svg-icons'
 
 // STYLES
 import './styles/category.scss';
 
+function moveRight(currentIndex) {
+    if(currentIndex === 2) {
+        currentIndex = 0;
+    } else {
+        currentIndex ++;
+    }
+    return currentIndex;
+}
+
+function moveLeft(currentIndex) {
+    if(currentIndex === 0) {
+        currentIndex = 2;
+    } else {
+        currentIndex --;
+    }
+    return currentIndex;
+}
+
 export default function Category(props) {
     const [ image, setImage ] = useState();
+    let currImageIndex = 0;
 
     useEffect(() => {
-        let currImageIndex = 0;
         setImage(props.category.images[currImageIndex]);
         setInterval(() => {
-            if(currImageIndex === 2) {
-                currImageIndex = 0;
-            } else {
-                currImageIndex ++;
-            }
+            currImageIndex = moveRight(currImageIndex);
             setImage(props.category.images[currImageIndex]);
         }, 5000);
     }, [])
+
+    const handleClick = (e) => {
+        if(e.target.name == "right") {
+            currImageIndex = moveRight(currImageIndex);
+            setImage(props.category.images[currImageIndex]);
+        } else {
+            currImageIndex = moveLeft(currImageIndex);
+            setImage(props.category.images[currImageIndex]);
+        }
+    }
 
     if(!image) {
         return <h1>Loading...</h1>
@@ -29,6 +55,10 @@ export default function Category(props) {
             <p className="description">{props.category.description}</p>
             <div className="image-container">
                 <img src={image}/>
+            </div>
+            <div className="arrow-container">
+                <FontAwesomeIcon onClick={handleClick} name="left" className="arrow-icon" icon={faArrowCircleLeft} size="3x" />
+                <FontAwesomeIcon onClick={handleClick} name="right" className="arrow-icon" icon={faArrowCircleRight} size="3x" />
             </div>
         </div>
     )
